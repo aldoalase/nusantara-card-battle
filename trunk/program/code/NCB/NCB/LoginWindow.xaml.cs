451dafd63f3de357ed2c17979a79ab24d11b8065
babibubabi@gmail.com <babibubabi@gmail.com@47e57b0d-7de3-ac4f-ed87-efabe2eccc6d>
2012-06-02 16:54:37 +0000

    + login logout

--- trunk/program/code/NCB/NCB/LoginWindow.xaml.cs
+++ trunk/program/code/NCB/NCB/LoginWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,32 +11,81 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NHibernate.Linq;
+
+using MySql.Data.MySqlClient;
+
 namespace NCB
 {
 	/// <summary>
 	/// Interaction logic for LoginWindow.xaml
 	/// </summary>
-	public partial class LoginWindow : Window
-	{
-		public LoginWindow()
-		{
-			this.InitializeComponent();
-			
-			// Insert code required on object creation below this point.
-		}
+    public partial class LoginWindow : Window
+    {
+        private List<Player> players = new List<Player>();
 
-		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-			MessageBox.Show("test");
-		}
+        public LoginWindow()
+        {
+            this.InitializeComponent();
 
-		private void ButtonExit_Click(object sender, System.Windows.RoutedEventArgs e)
-		{
-			// TODO: Add event handler implementation here.
-			this.Close();
-		}
+            // Insert code required on object creation below this point.
+        }
 
-		
-	}
+        private ISessionFactory CreateSessionFactory()
+        {
+            DbConnection conn = new DbConnection();
+            return Fluently.Configure()
+                .Database(conn.config())
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<PlayerMap>())
+                .BuildConfiguration().BuildSessionFactory();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // TODO: Add event handler implementation here.
+            MessageBox.Show("test");
+        }
+
+        private void ButtonExit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // TODO: Add event handler implementation here.
+            this.Close();
+        }
+
+        private void doLogin(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var factory = CreateSessionFactory();
+            using (var session = factory.OpenSession())
+            {
+                players = session.Query<Player>()
+                       //.OrderBy(c => c.PLAYER_NAME)
+                       .ToList();
+            }
+
+			bool authenticate = false;
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (TextBoxUsername.Text.Equals(players[i].PLAYER_NAME) && TextBoxPassword.Password.Equals(players[i].PLAYER_PASSWORD))
+                {
+                    authenticate = true;
+                    break;
+                }
+            }
+			if (authenticate)
+            {
+                Window menu = new MenuWindow();
+                menu.Show();
+                this.Hide();
+            }
+            else
+            {
+                TextBoxUsername.Text = "salah";
+            }
+        }
+    }
 }
