95ab2b047c16e37086dd1847648065e4f8769f70
babibubabi@gmail.com <babibubabi@gmail.com@47e57b0d-7de3-ac4f-ed87-efabe2eccc6d>
2012-06-03 03:33:14 +0000

    * fix DbConnection.cs * fix mapping method

--- trunk/program/code/NCB/NCB/DbConnection.cs
+++ trunk/program/code/NCB/NCB/DbConnection.cs
@@ -12,12 +12,37 @@ namespace NCB
 {
     class DbConnection
     {
-        public MySQLConfiguration config()
+        private MySQLConfiguration config()
         {
             return MySQLConfiguration.Standard.ConnectionString(c => c.Server("10.151.36.38")
                     .Database("tcgn")
                     .Username("tcgn")
                     .Password("tcgn"));
+        }
+
+        public ISessionFactory CreateSessionFactory(String map)
+        {
+            switch (map)
+            {
+                case "CardMap":
+                    return Fluently.Configure()
+                        .Database(config())
+                        .Mappings(m => m.FluentMappings.AddFromAssemblyOf<CardMap>())
+                        .BuildConfiguration().BuildSessionFactory();
+                case "PlayerMap" :
+                    return Fluently.Configure()
+                        .Database(config())
+                        .Mappings(m => m.FluentMappings.AddFromAssemblyOf<PlayerMap>())
+                        .BuildConfiguration().BuildSessionFactory();
+                case "Player_CardMap":
+                    return Fluently.Configure()
+                        .Database(config())
+                        .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Player_CardMap>())
+                        .BuildConfiguration().BuildSessionFactory();
+                default:
+                    return null;
+
+            }
         }
     }
 }
