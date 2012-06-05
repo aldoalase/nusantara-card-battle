e62a9d6941c07281788781d9ad9eb18f9d274a96
babibubabi@gmail.com <babibubabi@gmail.com@47e57b0d-7de3-ac4f-ed87-efabe2eccc6d>
2012-06-05 15:41:58 +0000

    * relocate all database connection method to DbConnection

--- trunk/program/code/NCB/NCB/Library/DbConnection.cs
+++ trunk/program/code/NCB/NCB/Library/DbConnection.cs
@@ -8,6 +8,9 @@ using FluentNHibernate.Cfg.Db;
 using NHibernate;
 using NCB;
 
+using NHibernate.Tool.hbm2ddl;
+using NHibernate.Linq;
+
 namespace NCB.Library
 {
     class DbConnection
@@ -48,6 +51,43 @@ namespace NCB.Library
                     return null;
 
             }
+        }
+
+        /* load player's card */
+        public List<Player_Card> loadPlayerCard(int playerId)
+        {
+            List<Player_Card> listCard;
+            var factory = CreateSessionFactory("Player_CardMap");
+            using (var session = factory.OpenSession())
+            {
+                listCard = session.Query<Player_Card>()
+                    .Where(u => u.Player.PLAYER_ID == playerId)
+                    .ToList();
+            }
+            return listCard;
+        }
+
+        public List<Player> login(string playerName, string playerPassword)
+        {
+            List<Player> listPlayer;
+            var factory = CreateSessionFactory("PlayerMap");
+            using (var session = factory.OpenSession())
+            {
+                listPlayer = session.Query<Player>()
+                    .Where(u => u.PLAYER_NAME.Equals(playerName))
+                    .Where(p => p.PLAYER_PASSWORD.Equals(playerPassword))
+                    .ToList();
+                /*
+                #simpe query
+                players = session.Query<Player>().ToList();
+                
+                #using HQl
+                IQuery query = session.CreateQuery("FROM Player p WHERE p.PLAYER_NAME = :Name");
+                query.SetParameter("Name", TextBoxUsername.Text);
+                players = query.List<Player>();
+                */
+            }
+            return listPlayer;
         }
     }
 }
