328b8ecf952ca0cf1b1fa131004795278986e8b1
babibubabi@gmail.com <babibubabi@gmail.com@47e57b0d-7de3-ac4f-ed87-efabe2eccc6d>
2012-06-07 14:03:48 +0000

    * fix DeckWindow

--- trunk/program/code/NCB/NCB/DeckWindow.xaml.cs
+++ trunk/program/code/NCB/NCB/DeckWindow.xaml.cs
@@ -21,13 +21,14 @@ namespace NCB
 	/// </summary>
 	public partial class DeckWindow : Window
 	{
-        public MenuWindow parent;
-        public Player player;
-        public List<Player_Card> cards = new List<Player_Card>();
-        public List<Player_Card> cardStock = new List<Player_Card>();
-        public List<Player_Card> activeDeck = new List<Player_Card>();
+        private MenuWindow parent;
+        private Player player;
+        private List<Player_Card> cards = new List<Player_Card>();
+        private List<Player_Card> cardStock = new List<Player_Card>();
+        private List<Player_Card> activeDeck = new List<Player_Card>();
         private int selectedCardStock = 0;
         private int selectedActiveDeck = 0;
+        private int maxDeck = 40;
 
         public DeckWindow(MenuWindow _parent, Player _player)
 		{
@@ -41,8 +42,15 @@ namespace NCB
 
         private void loadCard(int _playerId)
         {
+            //reset property
+            cards.Clear();
+            activeDeck.Clear();
+            cardStock.Clear();
+            selectedActiveDeck = 0; //this will reset activedesk back to first card
+            selectedCardStock = 0; //this will reset cardstock back to first card
+
             ModelPlayer_Card mpc = new ModelPlayer_Card();
-            cards = mpc.loadPlayerCard(_playerId);
+            cards = mpc.LoadPlayerCard(_playerId);
             activeDeck = cards.Where(item => item.PLAYER_CARD_ACTIVE).ToList();
             cardStock = cards.Where(item => !item.PLAYER_CARD_ACTIVE).ToList();
             /*
@@ -108,12 +116,26 @@ namespace NCB
 
         private void InsertDeckButton_Click(object sender, RoutedEventArgs e)
         {
-
+            //set current stock to active -> send to deck
+            if (cardStock.Count > 0 && activeDeck.Count < maxDeck)
+                this.Process(cardStock[selectedCardStock], true);
         }
 
         private void InsertStockButton_Click(object sender, RoutedEventArgs e)
         {
+            //set current deck to inactive -> send to stock
+            if(activeDeck.Count > 0)
+                this.Process(activeDeck[selectedActiveDeck], false);
+        }
 
+        private void Process(Player_Card current, bool val)
+        {
+            current.PLAYER_CARD_ACTIVE = val;
+
+            ModelPlayer_Card mpc = new ModelPlayer_Card();
+            mpc.Process("update", current);
+
+            this.loadCard(this.player.PLAYER_ID);
         }
 	}
 }
