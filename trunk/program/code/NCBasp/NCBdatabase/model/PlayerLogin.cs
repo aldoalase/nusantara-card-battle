5f58d24fd1ff0a655b3d447b623be9336d916f59
muhd.nad@gmail.com <muhd.nad@gmail.com@47e57b0d-7de3-ac4f-ed87-efabe2eccc6d>
2012-06-14 08:03:05 +0000

    + NCBasp (banyak)

---
+++ trunk/program/code/NCBasp/NCBdatabase/model/PlayerLogin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NHibernate.Linq;
+
+using MySql.Data.MySqlClient;
+
+using NCBdatabase;
+
+
+namespace NCBdatabase.model  
+{
+    public class PlayerLogin : connection
+    {
+        private ISessionFactory factory;
+
+        public PlayerLogin()
+        {
+            this.factory = this.CreateSessionFactory("Player_CardMap");
+        }
+
+        //public List<Player> Login(string _playerName)
+        public List<Player> Login(string _playerName, string _playerPassword)
+        {
+            List<Player> listPlayer = new List<Player>();
+            var factory = this.CreateSessionFactory("Player_CardMap");
+            using (var session = this.factory.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    listPlayer = session.Query<Player>()
+                        .Where(u => u.PLAYER_NAME.Equals(_playerName))
+                        .Where(p => p.PLAYER_PASSWORD.Equals(_playerPassword))
+                        .ToList();
+                    tx.Commit();
+                }
+            }
+            return listPlayer;
+            
+        }
+
+    }
+}
---
+++ trunk/program/code/NCBasp/NCBdatabase/model/RegisterPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Cfg;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Linq;
+
+using NCBdatabase;
+
+
+namespace NCBdatabase.model
+{
+    public class RegisterPlayer : connection
+    {
+        private ISessionFactory factory;
+
+        public RegisterPlayer()
+        {
+            
+        }
+
+        private void RegisPlayer()
+        {
+            this.factory = this.CreateSessionFactory("Player_CardMap");
+        }
+
+        public void RegisPlayer(string newUsername, string newPassword)
+        {
+            var factory = this.CreateSessionFactory("Player_CardMap");
+            using (var session = factory.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    var player = new Player
+                    {
+                        PLAYER_NAME = newUsername,
+                        PLAYER_PASSWORD = newPassword
+                    };
+
+                    session.Save(player);
+                    tx.Commit();
+                }
+            }
+        }
+    }
+}
+
+
