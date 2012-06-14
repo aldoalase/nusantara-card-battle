4b24251347fea1b60cb3076422a8128f1309f090
babibubabi@gmail.com <babibubabi@gmail.com@47e57b0d-7de3-ac4f-ed87-efabe2eccc6d>
2012-06-14 08:46:14 +0000

    * fix mapping

--- program/code/NCB/NCB/Mapping/Player_CardMap.cs
+++ program/code/NCB/NCB/Mapping/Player_CardMap.cs
@@ -13,7 +13,7 @@ namespace NCB
             Table("`player_card`");
             Id(x => x.PLAYER_CARD_ID);
             References<Card>(x => x.Card, "`CARD_ID`").Not.LazyLoad().Cascade.SaveUpdate();
-            References<Player>(x => x.Player, "`PLAYER_ID`").Cascade.SaveUpdate();
+            References<Player>(x => x.Player, "`PLAYER_ID`").Not.LazyLoad().Cascade.SaveUpdate();
             Map(x => x.PLAYER_CARD_ACTIVE);
         }
     }
