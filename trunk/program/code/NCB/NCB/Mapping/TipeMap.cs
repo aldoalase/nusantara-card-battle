4903ecadbc7ad702360424286eb827b67999fd34
babibubabi@gmail.com <babibubabi@gmail.com@47e57b0d-7de3-ac4f-ed87-efabe2eccc6d>
2012-06-14 09:51:05 +0000

    + ShopWindow

--- trunk/program/code/NCB/NCB/Mapping/TipeMap.cs
+++ trunk/program/code/NCB/NCB/Mapping/TipeMap.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FluentNHibernate.Mapping;
+using NCB.Mapping;
 
-namespace NCB.Mapping
+namespace NCB
 {
     public sealed class TipeMap : ClassMap<Tipe>
     {
