5bd9ef9e6fa7ca1c5342514db665b6df858be4de
s.g.nugraha@gmail.com <s.g.nugraha@gmail.com@47e57b0d-7de3-ac4f-ed87-efabe2eccc6d>
2012-06-18 15:41:37 +0000

    * ngedit profile aspx

--- trunk/program/code/NCBasp/NCBdatabase/mapped/BattleMap.cs
+++ trunk/program/code/NCBasp/NCBdatabase/mapped/BattleMap.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FluentNHibernate.Mapping;
-using NCB.Mapping;
 
-namespace NCB
+namespace NCBdatabase
 {
     public sealed class BattleMap : ClassMap<Battle>
     {
