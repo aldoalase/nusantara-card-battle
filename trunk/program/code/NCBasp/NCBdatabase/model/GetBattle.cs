5d29bad3dc09bc6d27693ecda840fdc9e75d0092
s.g.nugraha@gmail.com <s.g.nugraha@gmail.com@47e57b0d-7de3-ac4f-ed87-efabe2eccc6d>
2012-06-18 16:33:40 +0000

    * habis otak atik last battle + GetBattle.cs

---
+++ trunk/program/code/NCBasp/NCBdatabase/model/GetBattle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace NCBdatabase.model
+{
+    public class GetBattle : connection
+    {
+        private ISessionFactory factory;
+
+        public GetBattle()
+        {
+            
+        }
+
+        public List<Battle> GetBattles(Player player)
+        {
+            this.factory = this.CreateSessionFactory("Player_CardMap");
+            List<Battle> listBattle = new List<Battle>();
+            using (var session = this.factory.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    listBattle = session.Query<Battle>()
+                        .Where(u => u.BATTLE_PLAYER_1.Equals(player.PLAYER_ID))
+                        .ToList();
+                    tx.Commit();
+                }
+            }
+            return listBattle;
+        }
+    }
+}
