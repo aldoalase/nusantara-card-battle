fb1ae88ade6747c231515b1ba9b41eb4bb836d6e
s.g.nugraha@gmail.com <s.g.nugraha@gmail.com@47e57b0d-7de3-ac4f-ed87-efabe2eccc6d>
2012-06-20 05:33:28 +0000

    * ngedit UbahPassword

--- trunk/program/code/NCBasp/NCBasp/UbahPassword.aspx.designer.cs
+++ trunk/program/code/NCBasp/NCBasp/UbahPassword.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace NCBasp {
         protected global::System.Web.UI.WebControls.TextBox UbahPassBox3;
         
         /// <summary>
+        /// Status control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Status;
+        
+        /// <summary>
         /// UbahPassButton control.
         /// </summary>
         /// <remarks>
